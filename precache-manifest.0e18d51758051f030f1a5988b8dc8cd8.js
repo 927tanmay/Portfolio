9a5e04b4b4cf7206aed5e4c34d4dc18e4d5c550a
927tanmay <927tanmay@gmail.com>
2021-07-19 13:37:26 +0530

    Updates

---
+++ precache-manifest.0e18d51758051f030f1a5988b8dc8cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "692c82b41924f53de30e",
+    "url": "/portfolio/static/css/main.d3ff050e.chunk.css"
+  },
+  {
+    "revision": "692c82b41924f53de30e",
+    "url": "/portfolio/static/js/main.692c82b4.chunk.js"
+  },
+  {
+    "revision": "886cc40fa1c73aa0688c",
+    "url": "/portfolio/static/js/1.886cc40f.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "9768816b582e821e0c65ced69cd5bdeb",
+    "url": "/portfolio/static/media/RESUME_TANMAY_SHARMA.9768816b.pdf"
+  },
+  {
+    "revision": "d4979b57be33eb51be985751c5b3502f",
+    "url": "/portfolio/index.html"
+  }
+];
